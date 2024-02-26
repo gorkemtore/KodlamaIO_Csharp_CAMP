5929cea5416cb79cf6da74e05336d75cb413f6ce
gorkemtore <gorkemtore1@gmail.com>
2024-02-26 12:55:03 +0300

    WebApi ders sonu

---
+++ WebApi/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Dtos.Responses;
+using Business.Dtos.Requests;
+using Business.Abstracts;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreateBrandRequest createBrandRequest)
+        {
+            CreatedBrandResponse createdBrandResponse = _brandService.Add(createBrandRequest);
+            return Ok(createdBrandResponse);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_brandService.GetAll());
+        }
+    }
+}
